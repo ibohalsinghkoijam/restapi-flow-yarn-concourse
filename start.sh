e4ce4a5121d841425a93ac58120d6b3fbe01d6d4
ibohalsinghkoijam <koijam@olivetech.net>
2019-08-06 16:40:10 +0530

    node testing

--- start.sh
+++ start.sh
@@ -8,10 +8,10 @@ echo "Concourse API target ${fly_target}"
 echo "Concourse $(basename $DIR)"
 
 pushd "$DIR"
-  fly -t ${fly_target} validate-pipeline --config pipeline.yml
-  fly -t ${fly_target} set-pipeline -p restapi-pipeline -c ci/pipeline.yml -n
-  fly -t ${fly_target} unpause-pipeline -p restapi-pipeline
+  fly -t ${fly_target} validate-pipeline --config pipeline-test.yml
+  fly -t ${fly_target} set-pipeline -p test-pipeline -c ci/pipeline-test.yml -n
+  fly -t ${fly_target} unpause-pipeline -p test-pipeline
   #fly -t ${fly_target} trigger-job -w -j sample-pipeline/job-run-feature
   #fly -t ${fly_target} trigger-job -w -j sample-pipeline/job-run-develop
-  fly -t ${fly_target} trigger-job -w -j restapi-pipeline/job-run-develop
+  fly -t ${fly_target} trigger-job -w -j test-pipeline/job-run-master
 popd
