7651f9fc91d7bd357411ab42e5704a904e9890e2
ibohalsinghkoijam <koijam@olivetech.net>
2019-08-06 18:22:40 +0530

    more node server issue

---
+++ test/testnode.js
@@ -0,0 +1,18 @@
+var mocha = require('mocha');
+var request = require('supertest');
+var server = require('server');
+
+describe('test server.js', function() {
+
+    it('test GET /', function(done) {
+        request(server)
+            .get('/')
+            .expect(200, done);
+    });
+
+    after(function (done) {
+        done();
+       process.exit(1);
+ })
+
+});
