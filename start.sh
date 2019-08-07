3065514a714e7058535cafe3ade4e3a1502570d5
ibohalsinghkoijam <koijam@olivetech.net>
2019-08-07 11:13:20 +0530

    more changes

--- start.sh
+++ start.sh
@@ -6,10 +6,11 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export fly_target=${fly_target:-devops}
 echo "Concourse API target ${fly_target}"
 echo "Concourse $(basename $DIR)"
+export branchName=$(git branch | grep \* | cut -d ' ' -f2)
 
 pushd "$DIR"
-  fly -t ${fly_target} validate-pipeline --config ci/pipeline.yml
-  fly -t ${fly_target} set-pipeline -p restapi-pipeline -c ci/pipeline.yml -n
+  fly -t ${fly_target} validate-pipeline --config pipeline.yml
+  fly -t ${fly_target} set-pipeline -p restapi-pipeline -c pipeline.yml --load-vars-from=credentials.yml -v branch-version=${branchName} -n
   fly -t ${fly_target} unpause-pipeline -p restapi-pipeline
   #fly -t ${fly_target} trigger-job -w -j sample-pipeline/job-run-feature
   #fly -t ${fly_target} trigger-job -w -j sample-pipeline/job-run-develop
