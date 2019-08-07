752315284ba0d0511cb5b85b0e0f109c1707b036
ibohalsinghkoijam <koijam@olivetech.net>
2019-08-07 14:40:31 +0530

    refactoring code

--- ci/start.sh
+++ ci/start.sh
@@ -9,10 +9,11 @@ echo "Concourse $(basename $DIR)"
 export branchName=$(git branch | grep \* | cut -d ' ' -f2)
 
 pushd "$DIR"
-  fly -t ${fly_target} validate-pipeline --config ci/pipeline.yml
-  fly -t ${fly_target} set-pipeline -p restapi-pipeline -c ci/pipeline.yml --load-vars-from=ci/credentials.yml -v branch-version=${branchName} -n
+  fly -t ${fly_target} validate-pipeline --config pipeline.yml
+  fly -t ${fly_target} set-pipeline -p restapi-pipeline -c pipeline.yml --load-vars-from=credentials.yml -v branch-version=${branchName} -n
   fly -t ${fly_target} unpause-pipeline -p restapi-pipeline
   #fly -t ${fly_target} trigger-job -w -j sample-pipeline/job-run-feature
   #fly -t ${fly_target} trigger-job -w -j sample-pipeline/job-run-develop
+  fly -t ${fly_target} trigger-job -w -j restapi-pipeline/job-run-master
   fly -t ${fly_target} trigger-job -w -j restapi-pipeline/job-run-master
 popd
